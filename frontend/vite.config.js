e44561d88ac81ac5bb0007210b3e615a442d8cc8
First Last <mallinadhanu@gmail.com>
2026-05-22 01:32:56 +0530

    Raise Vite chunkSizeWarningLimit to 2 MB

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    chunkSizeWarningLimit: 2000 // increase limit to 2 MB to suppress warnings
+  }
+});
